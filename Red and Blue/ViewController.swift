440db9d0449d2257b5beeb89d5fe2a63bfc779e5
Kyle Johannsen <kylejohannsen@kyles-air.attlocal.net>
2016-05-28 10:53:23 -0400

    Added files for Red and Blue

--- Red and Blue/ViewController.swift
+++ Red and Blue/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redButton: UIButton!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var bothButton: UIButton!
+    @IBOutlet weak var resetButton: UIButton!
+    @IBOutlet weak var redImage: UIImageView!
+    @IBOutlet weak var blueImage: UIImageView!
+    @IBOutlet weak var bothImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,27 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideRed(sender: AnyObject) {
+        redImage.hidden = true
+        bothImage.hidden = true
 
+    }
+
+    @IBAction func hideBlue(sender: AnyObject) {
+        blueImage.hidden = true
+        bothImage.hidden = true
+    }
+    
+    @IBAction func showBoth(sender: AnyObject) {
+        redImage.hidden = true
+        blueImage.hidden = true
+        bothImage.hidden = false
+    }
+    
+    @IBAction func resetButton(sender: AnyObject) {
+        blueImage.hidden = false
+        redImage.hidden = false
+        bothImage.hidden = true
+    }
 }
 
